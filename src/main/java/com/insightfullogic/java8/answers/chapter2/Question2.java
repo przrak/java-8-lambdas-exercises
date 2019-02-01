0cc6f7a40d1da3801dbbde6303442ae0d3a9df01
Dmitry <przrak@gmail.com>
2019-02-01 18:17:10 +0700

    HW

--- src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
+++ src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
@@ -5,8 +5,10 @@ import javax.swing.text.DateFormatter;
 
 import static java.lang.ThreadLocal.withInitial;
 
-public class Question2 {
+public class Question2
+{
 
-     public final static ThreadLocal<DateFormatter> formatter = ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy")));
+    public final static ThreadLocal<DateFormatter> formatter =
+        ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy")));
 
 }
--- src/main/java/com/insightfullogic/java8/exercises/chapter2/Question2.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter2/Question2.java
@@ -2,13 +2,25 @@ package com.insightfullogic.java8.exercises.chapter2;
 
 import com.insightfullogic.java8.exercises.Exercises;
 
+import javax.swing.*;
 import javax.swing.text.DateFormatter;
+
+import java.text.SimpleDateFormat;
 
 import static java.lang.ThreadLocal.withInitial;
 
-public class Question2 {
+public class Question2
+{
 
     public static ThreadLocal<DateFormatter> formatter
-            = Exercises.replaceThisWithSolution();
+        = ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy")));
+
+    public static void main(String[] args)
+    {
+        JButton button = new JButton();
+        button.addActionListener(event ->
+            System.out.println(event.getActionCommand()));
+    }
+
 
 }
